101243ac260767c6bca4dd656b491ec5212b90c5
Buddhika Shaminda Rathnayaka <rmbsrathnayaka@gmail.com>
2017-12-04 16:31:10 +0530

    Profile

---
+++ application/controllers/Upload.php
@@ -0,0 +1,37 @@
+<?php
+
+class Upload extends CI_Controller {
+
+ 
+        public function index()
+        {
+                $error="";
+                $this->load->view('cimage', $error);
+        }
+
+        public function do_upload()
+        {
+                $config['upload_path']          = './uploads/';
+                $config['allowed_types']        = 'gif|jpg|png|jpeg';
+                $config['max_size']             = 10000;
+                $config['max_width']            = 20240;
+                $config['max_height']           = 76800;
+
+                $this->load->library('upload', $config);
+
+                if ( ! $this->upload->do_upload('userfile'))
+                {
+                        $error = array('error' => $this->upload->display_errors());
+                        echo "error";
+                        $this->load->view('cimage', $error);
+
+                }
+                else
+                {
+                        $data = array('upload_data' => $this->upload->data());
+
+                        $this->load->view('upload_success', $data);
+                }
+        }
+}
+?>
